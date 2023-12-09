206d9feab13f0813d234389ad8df4fe3df233026
Ian <ian@dominionintellect.com>
2023-12-08 23:35:09 -0800

    added loader for testing

---
+++ loader.lua
@@ -0,0 +1,56 @@
+local http = require("internet")
+local fs = require("filesystem")
+
+function webRequest(url)
+    -- Returns entire block from request site
+    local response = ""
+    for chunk in http.request(url) do
+        response = response .. chunk
+    end
+
+    return response
+end
+
+function parseTagName(jsonString)
+    -- Pattern to find the tag_name value
+    local pattern = '"tag_name":%s*"(.-)"'
+
+    -- Find and return the tag_name
+    local start, stop, value = string.find(jsonString,pattern)
+    if value then
+        return value
+    else
+        return nil
+    end
+end
+
+function updateSystem(version)
+    -- updates system to provided version
+    local file = open("loader.lua","w")
+    
+    file:write(webRequest(string.format("https://raw.githubusercontent.com/summitninja/mc-lua-test/%s/loader.lua",version)))
+    open(".version","w"):write(version)
+
+end
+
+
+-- get the latest release
+local r = webRequest("https://api.github.com/repos/summitninja/mc-lua-test/releases/latest")
+
+local release = parseTagName(r)
+
+
+
+-- Check if latest release was gotten
+if release ~= nil then
+-- Try to read and compare it to the .version file
+    if fs.exists(".version") then
+        local version = io.open(".version","r"):read()
+        if version == release then
+            print("Running latest version!\n%s",release)
+            return
+        end
+        updateSystem(release)
+        print("Updated system to version %s",release)
+    end
+end
